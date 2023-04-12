9f55e4081e7979ef7c1e2d9848af855057bb35e0
Robmel12 <rob.melnyk@gmail.com>
2023-04-12 17:59:15 -0600

    new plan

--- nav.js
+++ nav.js
@@ -1,37 +1,4 @@
-
-import {applyDelay, animateFade, animateMoveIn, animateMoveOut, animateRotate} from './animation.js';
-
-function imgBackgroundChange(event){
-    event.preventDefault();
-    const image = document.getElementById('featured-image');
-    const imageBackground = document.getElementById('featured-image-background');
-    imageBackground.classList.toggle('hover');
-    image.classList.toggle('shrink');
+import { rotate } from "./animation.js";
+function toggleNav(){
+    console.log('toggle');
 }
-function toggleMainNav(event){
-    event.preventDefault();
-
-    const hamburger = document.getElementById('hamburger');
-   
-    const mainNav = document.querySelector('#main-nav');
-    const callToAction = document.querySelectorAll('.call-to-action.phone, .call-to-action.message');
-    const featuredImageOverflow = document.getElementById('featured-image-overflow');
-    hamburger.classList.toggle('active');
-    mainNav.classList.toggle('active');
-    animateMoveIn('#main-nav');
-    animateRotate('.nav-menu')
-    if(hamburger.classList.contains('active')){
-        applyDelay('.nav-menu', '.nav-link', 'normal', .2)
-    }else{
-        applyDelay('.nav-menu', '.nav-link', 'reverse', .2)
-    }
-    featuredImageOverflow.classList.toggle('active');
-    callToAction.forEach(element => {
-        element.classList.toggle('active');          
-    });
-    
-    
-}
-// window.addEventListener('onload')
-window.toggleMainNav = toggleMainNav;
-window.imgBackgroundChange = imgBackgroundChange;
