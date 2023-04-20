3bbb654ac2e5fb4f1fda128e74514334f6d9328b
Robmel12 <rob.melnyk@gmail.com>
2023-04-20 14:38:49 -0600

    Starting to build

--- modules/animation.js
+++ modules/animation.js
@@ -1,17 +1,111 @@
+function getElements(){
+  switch (arguments.length){
+    case 1:
+        return document.querySelector(arguments[0])
+    case 2:
+        if(arguments[1] == true){
+            return document.querySelectorAll(arguments[0]);
+        }else{
+            return document.querySelector(arguments[0]).querySelectorAll(arguments[1]);
+        }   
+        
+    default: return []
+    }
+  }
 
-function getElements(containerClass, className){
-    return document.querySelector(containerClass)?.querySelectorAll(className);
+export function createToggle(){
+    let elements;
+    let toggleClass;
+    const forEachFunc = (element)=>{
+        element.classList.toggle(toggleClass)
+    }
+    switch(arguments.length){
+        case 2:
+            toggleClass = arguments[1]
+           elements = getElements(arguments[0]).classList.toggle(toggleClass);
+        case 3:
+            if(arguments[2] === true){
+                toggleClass = arguments[1];
+                elements = getElements(arguments[0], arguments[2]).forEach(forEachFunc)
+            }
+            toggleClass = arguments[2]
+            elements = getElements(arguments[0], arguments[1]).forEach(forEachFunc)
+            break
+            default: console.error('must have 2-3 arguments for createToggle function.')
+        }
+
+
+}
+function createDelayTime(elmObj, ratio = 0, hold = 0){
+   return elmObj.length !== undefined ? elmObj.forEach((element, index) =>    element.style.transitionDelay = index*ratio + hold + 's'): elmObj.style.transitionDelay = 1 *ratio + hold + 's';
+
+   
+}
+function createDelayTimeReverse(elmObj, ratio = 0, hold= 0){
+   return elmObj.length !== undefined ? elmObj.forEach((element, index) => element.style.transitionDelay = (elmObj.length - index)*ratio + hold +'s'): elmObj.style.transitionDelay = 1*ratio + hold +'s';
+   
+}
+export function delayElements(){
+    
+
+    if(Object.values(arguments).includes('reverse')){
+    switch(arguments.length){
+        case 3:
+            createDelayTimeReverse(getElements(arguments[0], true), arguments[1]); 
+            break
+        case 4:
+            if(typeof arguments[1] === 'string'){
+                createDelayTimeReverse(getElements(arguments[0], arguments[1]), arguments[2]);
+            break
+            }
+            createDelayTimeReverse(getElements(arguments[0], true), arguments[1], arguments[2]);
+            break
+        case 5:createDelayTimeReverse(getElements(arguments[0], arguments[1]), arguments[2], arguments[3]);
+            break
+    }    
+}else{
+   switch(arguments.length){
+    case 2:
+        createDelayTime(getElements(arguments[0]), arguments[1]);
+        break
+    case 3:if(arguments[2] === true){
+        createDelayTime(getElements(arguments[0], true), arguments[1]);
+        break
+    }else if(typeof arguments[1] === 'string'){
+        console.log('called it!')
+        createDelayTime(getElements(arguments[0], arguments[1]), arguments[2])
+        break
+    }else{
+        createDelayTime(getElements(arguments[0]), arguments[1], arguments[2]);
+        break
+    }
+    case 4:
+        if(arguments[3] === true){
+            createDelayTime(getElements(arguments[0]), arguments[1], arguments[2])
+        }else{
+            createDelayTime(getElements(arguments[0], arguments[1]), arguments[2], arguments[3]);
+        }
+   }
 }
 
-export function hide(containerClass = body, className){
-    getElements(containerClass, className).forEach(element => {
-        element.style.display = 'none'
-       });
-}
 
-export function rotate(containerClass ='body', className, degree ='', time = 0, delay = 0, transitionFlow =''){
-   getElements(containerClass, className).forEach(element => {
-    element.style.rotate= degree;
-    element.style.transition = time+'s '+transitionFlow 
-   });
+
+}
+export function applyTransition(){
+let elements;
+    switch(arguments.length){
+    case 3:
+        elements = getElements(arguments[0]);
+        elements.length !== undefined? elements.forEach(element=> element.style[arguments[1]] = arguments[2]):elements.style[arguments[1]] = arguments[2];
+        break
+    case 4:
+        if(arguments[1] === true){
+            elements =  getElements(arguments[0], true);
+            elements.forEach(element => element.style[arguments[2]] = arguments[3] )
+            break
+        }
+        elements = getElements(arguments[0], arguments[1]);
+        elements.forEach(element => element.style[arguments[2]] = arguments[3])
+
+}
 }
--- nav.js
+++ nav.js
@@ -1,6 +1,55 @@
-import { hide, rotate } from "./animation.js";
-function toggleNav(){
-    rotate('.nav-menu', 'li', '15deg', .2);
-    hide('.nav-menu', 'li');
-}
-window.toggleNav = toggleNav;
+import { createToggle, delayElements, applyTransition } from "./modules/animation.js";
+
+function toggleNav(containerClass, className, subClass){
+        let clicked = false;
+        this.toggle = function(event){
+            event.preventDefault();
+            clicked = !clicked;
+            if(clicked){
+                delayElements(className, subClass, .2, .5);
+                delayElements(containerClass, className, 0)
+                
+            }else{
+                delayElements(containerClass, className, 1, 1)
+                delayElements(className, subClass, .2,'reverse');
+                
+            }
+            createToggle(containerClass, className, 'active');
+            createToggle(className, subClass, 'active');
+          
+        }  ; 
+            
+    }
+
+    const toggleNavMenu = new toggleNav('.nav-menu-container', '.nav-menu', 'li');
+    
+    window.addEventListener('load', function(event){
+        event.preventDefault();
+        const imageContainerClass = '.home-image-container'
+        const curtain = '.curtain';
+        const backgroundImage = '.home-background-image';
+        const wordsContainerClass = '.home-words-container';
+        const imageClass = 'img';
+        const lettersClass = ' h1';
+        const toggleClass = 'active';
+        
+        //Home words toggle delay
+        delayElements(wordsContainerClass, lettersClass, .2, 2);
+        createToggle(wordsContainerClass, lettersClass, toggleClass);
+        
+        //image toggle delay
+        delayElements(imageClass, 2)
+        this.setTimeout(()=>{delayElements(imageClass,0)}, 2)
+        createToggle(backgroundImage, toggleClass)
+        createToggle(imageClass, toggleClass);
+        createToggle(curtain, toggleClass);
+        createToggle(imageContainerClass, toggleClass);
+        delayElements(imageContainerClass, 2);
+        delayElements(curtain, 1);
+        delayElements(backgroundImage, 1);
+        this.setTimeout(()=>{delayElements(backgroundImage,0)}, 1)
+        this.setTimeout(()=>{delayElements(backgroundImage,0)}, 1   )
+        
+    })
+    window.toggleNav = toggleNavMenu.toggle;
+
